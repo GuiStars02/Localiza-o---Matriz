653f3eaf08880075488f9d1f3a38aca236f7378a
Guilherme Andrade Campos <guilhermeandrade.2025@gmail.com>
2024-07-08 22:48:52 -0300

    slight update

--- Program.cs
+++ Program.cs
@@ -4,27 +4,29 @@ namespace MyApp {
     internal class Program {
         static void Main(string[] args) {
 
+            Console.WriteLine("Digite o tamanho da sua matriz separado por um espaço: ");
             string[] line = Console.ReadLine().Split(' ');
             int m = int.Parse(line[0]);
             int n = int.Parse(line[1]);
 
             int[,] mat = new int[m, n];
 
+            Console.WriteLine("Digite os números separados com um espaço: ");
             for (int i = 0; i < m; i++) {
                 string[] array = Console.ReadLine().Split(" ");
                 for (int j = 0; j < n; j++) {
                     mat[i, j] = int.Parse(array[j]);
                 }
             }
-
+            Console.WriteLine("Qual número você busca? ");
             int number = int.Parse(Console.ReadLine());
 
+            Console.WriteLine("\nDados: ");
             for (int i = 0; i < m; i++) {
                 for (int j = 0; j < n; j++) {
 
                     if (mat[i, j] == number) {
                         Console.WriteLine($"Position {i},{j}:");
-                        int var;
 
                         if (j > 0) {
                             Console.WriteLine("Left:" + mat[i, j - 1]);
